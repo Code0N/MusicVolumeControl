05fa39c16d8ab4f340195a8811aea8667820a704
Code0N <redeemer.of.souls@yandex.ru>
2018-11-27 16:28:08 +0300

    Some UI and code improvements

--- app/src/main/java/code0n/musicvolumecontrol/MainActivity.java
+++ app/src/main/java/code0n/musicvolumecontrol/MainActivity.java
@@ -13,24 +13,26 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Context conxt = this;
+    AudioManager audioManager;
     String[] volLevels = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) conxt.getSystemService(Context.AUDIO_SERVICE);
         ArrayAdapter<String> VLAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, volLevels);
         VLAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(VLAdapter);
         spinner.setPrompt("Выберите уровень");
-        spinner.setSelection(getSoundValue(conxt));
+        spinner.setSelection(getSoundValue(audioManager));
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 //Toast.makeText(getBaseContext(), "Позиция = " + position, Toast.LENGTH_SHORT).show();
-                setSoundValue(conxt, position);
+                setSoundValue(audioManager, position);
             }
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
@@ -38,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public static void setSoundValue(Context context, int value) {
-        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    public static void setSoundValue(AudioManager audioManager, int value) {
+        //AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, value, 0);
     }
 
-    public static int getSoundValue(Context context) {
-        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    public static int getSoundValue(AudioManager audioManager) {
+        //AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
 }
